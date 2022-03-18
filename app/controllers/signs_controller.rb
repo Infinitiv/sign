6645eabaec6e32d43382d4cd6c1b845890259efc
Vladimir Markovnin <markovnin@isma.ivanovo.ru>
2022-03-18 09:19:20 +0000

    fix env

--- app/controllers/signs_controller.rb
+++ app/controllers/signs_controller.rb
@@ -7,7 +7,7 @@ class SignsController < ApplicationController
     filename = Digest::MD5.hexdigest(header_payload)
     %x(mkdir -p "#{path}")
     %x(echo "#{header_payload}" >> "#{path}/#{filename}")
-    %x(/opt/cprocsp/bin/amd64/cryptcp -signf -dir "#{path}" -thumbprint RAILS_ENV['THUMBPRINT'] -pin RAILS_ENV['PIN'] -hashalg 1.2.643.7.1.1.2.2 -detached "#{path}/#{filename}")
+    %x(/opt/cprocsp/bin/amd64/cryptcp -signf -dir "#{path}" -thumbprint "#{ENV['THUMBPRINT']}" -pin "#{ENV['PIN']}" -hashalg 1.2.643.7.1.1.2.2 -detached "#{path}/#{filename}")
     sign = %x(cat "#{path}/#{filename}.sgn").gsub!(/\s+/, '')
     send_data({sign: sign}.to_json)
     %x(rm "#{path}/#{filename}")
