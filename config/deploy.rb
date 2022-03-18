0809e5f2a8cde65e990b180c4e2da723f0caf759
Vladimir Markovnin <markovnin@isma.ivanovo.ru>
2022-03-18 09:57:01 +0000

    fix branch name

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ set :application, "sign"
 set :repo_url, "git@github.com:Infinitiv/sign.git"
 
 # Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, "/home/ubuntu/www/sign"
