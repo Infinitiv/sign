025b23c74f2c8534d250f3ce027f7b3d7ade2596
Vladimir Markovnin <markovnin@isma.ivanovo.ru>
2022-03-18 09:03:19 +0000

    initial commit

---
+++ app/controllers/signs_controller.rb
@@ -0,0 +1,16 @@
+class SignsController < ApplicationController
+  require 'digest'
+  
+  def create
+    path = "/home/ubuntu/signs"
+    header_payload = params[:message]
+    filename = Digest::MD5.hexdigest(header_payload)
+    %x(mkdir -p "#{path}")
+    %x(echo "#{header_payload}" >> "#{path}/#{filename}")
+    %x(/opt/cprocsp/bin/amd64/cryptcp -signf -dir "#{path}" -thumbprint RAILS_ENV['THUMBPRINT'] -pin RAILS_ENV['PIN'] -hashalg 1.2.643.7.1.1.2.2 -detached "#{path}/#{filename}")
+    sign = %x(cat "#{path}/#{filename}.sgn").gsub!(/\s+/, '')
+    send_data({sign: sign}.to_json)
+    %x(rm "#{path}/#{filename}")
+    %x(rm "#{path}/#{filename}".sgn)
+  end
+end
