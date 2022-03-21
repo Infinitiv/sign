4ae5f943459a4d2da3fb15f85a009f62e301affe
Vladimir Markovnin <markovnin@isma.ivanovo.ru>
2022-03-21 07:29:43 +0000

    some changes

--- app/controllers/signs_controller.rb
+++ app/controllers/signs_controller.rb
@@ -7,7 +7,7 @@ class SignsController < ApplicationController
     filename = Digest::MD5.hexdigest(header_payload)
     %x(mkdir -p "#{path}")
     %x(echo "#{header_payload}" >> "#{path}/#{filename}")
-    %x(/opt/cprocsp/bin/amd64/cryptcp -signf -dir "#{path}" -thumbprint "#{ENV['THUMBPRINT']}" -pin "#{ENV['PIN']}" -detached "#{path}/#{filename}")
+    %x(/opt/cprocsp/bin/amd64/cryptcp -sign -thumbprint "#{ENV['THUMBPRINT']}" -pin "#{ENV['PIN']}" -detached "#{path}/#{filename}" "#{path}/#{filename}.sgn")
     sign = %x(cat "#{path}/#{filename}.sgn").gsub!(/\s+/, '')
     send_data({sign: sign}.to_json)
     %x(rm "#{path}/#{filename}")
