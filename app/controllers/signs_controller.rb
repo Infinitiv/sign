ff76e53e82a443b5d39377e9e9cd31d064ad4d45
Vladimir Markovnin <markovnin@isma.ivanovo.ru>
2022-03-21 10:40:38 +0000

    some changes

--- app/controllers/signs_controller.rb
+++ app/controllers/signs_controller.rb
@@ -7,11 +7,10 @@ class SignsController < ApplicationController
     filename = Digest::MD5.hexdigest(header_payload)
     %x(mkdir -p "#{path}")
     %x(echo "#{header_payload}" >> "#{path}/#{filename}")
-    %x(/opt/cprocsp/bin/amd64/cryptcp -sign -thumbprint "#{ENV['THUMBPRINT']}" -pin "#{ENV['PIN']}" -cert -detached "#{path}/#{filename}" "#{path}/#{filename}.sig")
-#     %x(/opt/cprocsp/bin/amd64/csptest -sfsign -sign -add -detached -base64 -my "#{ENV['THUMBPRINT']}" -password "#{ENV['PIN']}" -in "#{path}/#{filename}" -out "#{path}/#{filename}.sig")
-    sign = %x(cat "#{path}/#{filename}.sig").gsub!(/\s+/, '')
+    %x(/opt/cprocsp/bin/amd64/cryptcp -signf -dir "#{path}" -thumbprint "#{ENV['THUMBPRINT']}" -pin "#{ENV['PIN']}" -detached "#{path}/#{filename}")
+    sign = %x(cat "#{path}/#{filename}.sgn").gsub!(/\s+/, '')
     send_data({sign: sign}.to_json)
-#     %x(rm "#{path}/#{filename}")
-#     %x(rm "#{path}/#{filename}".sig)
+    %x(rm "#{path}/#{filename}")
+    %x(rm "#{path}/#{filename}".sgn)
   end
 end
