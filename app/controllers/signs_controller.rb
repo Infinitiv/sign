3e896ab9e957de7faf87f6baee05a785b0d7b651
Vladimir Markovnin <markovnin@isma.ivanovo.ru>
2022-07-08 19:27:44 +0000

    fils

--- app/controllers/signs_controller.rb
+++ app/controllers/signs_controller.rb
@@ -7,16 +7,13 @@ class SignsController < ApplicationController
     filename = Digest::MD5.hexdigest(header_payload)
     %x(mkdir -p "#{path}")
     if params[:message]
-      %x(echo "#{header_payload}" >> "#{path}/#{filename}")
+      %x(printf "#{header_payload}" >> "#{path}/#{filename}")
     else
       File.open("#{path}/#{filename}", 'wb'){|f| f.write(Base64.decode64(header_payload))}
     end
     %x(/opt/cprocsp/bin/amd64/cryptcp -signf -dir "#{path}" -cert -detached -thumbprint "#{ENV['THUMBPRINT']}" -pin "#{ENV['PIN']}" "#{path}/#{filename}")
-    sign = %x(cat "#{path}/#{filename}.sgn").gsub!(/\s+/, '')
+    sign = File.read("#{path}/#{filename}.sgn").gsub(/\s+/, '')
     send_data({sign: sign}.to_json)
-#     %x(/opt/cprocsp/bin/amd64/csptest -sfsign -sign -add -base64 -detached -password "#{ENV['PIN']}" -in "#{path}/#{filename}" -out "#{path}/#{filename}.sig" -my "#{ENV['THUMBPRINT']}")
-#     sign = %x(cat "#{path}/#{filename}.sig").gsub!(/\s+/, '')
-#     send_data({sign: sign}.to_json)
     %x(rm "#{path}/#{filename}")
     %x(rm "#{path}/#{filename}".sgn)
   end
