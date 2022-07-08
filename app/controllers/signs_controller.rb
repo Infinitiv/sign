b10c1b166e18a62f06733fad3c873eeaae6228bc
Vladimir Markovnin <markovnin@isma.ivanovo.ru>
2022-07-08 18:30:53 +0000

    file

--- app/controllers/signs_controller.rb
+++ app/controllers/signs_controller.rb
@@ -3,10 +3,20 @@ class SignsController < ApplicationController
   
   def create
     path = "/home/ubuntu/signs"
-    header_payload = params[:message]
+    header_payload = case true
+                      when params[:message]
+                        params[:message]
+                      when params[:file]
+                        params[:file]
+                      end
     filename = Digest::MD5.hexdigest(header_payload)
     %x(mkdir -p "#{path}")
-    %x(echo "#{header_payload}" >> "#{path}/#{filename}")
+    case true
+    when params[:message]
+      %x(echo "#{header_payload}" >> "#{path}/#{filename}")
+    when params[:file]
+      %x(echo "#{Base64.decode64(header_payload)}" >> "#{path}/#{filename}")
+    end
     %x(/opt/cprocsp/bin/amd64/cryptcp -signf -dir "#{path}" -cert -detached -thumbprint "#{ENV['THUMBPRINT']}" -pin "#{ENV['PIN']}" "#{path}/#{filename}")
     sign = %x(cat "#{path}/#{filename}.sgn").gsub!(/\s+/, '')
     send_data({sign: sign}.to_json)
