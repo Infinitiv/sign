d0d762b3e0d10ec4944355f1059407a5484fcfd9
Vladimir Markovnin <markovnin@isma.ivanovo.ru>
2022-07-11 08:42:53 +0000

    fix cert

--- app/controllers/signs_controller.rb
+++ app/controllers/signs_controller.rb
@@ -5,13 +5,14 @@ class SignsController < ApplicationController
     path = "/home/ubuntu/signs"
     header_payload = params[:message] ? params[:message] : params[:file]
     filename = Digest::MD5.hexdigest(header_payload)
+    thumbprint = params[:message] ? ENV['THUMBPRINT'] : ENV['THUMBPRINT_PERSONAL']
     %x(mkdir -p "#{path}")
     if params[:message]
       %x(printf "#{header_payload}" >> "#{path}/#{filename}")
     else
       File.open("#{path}/#{filename}", 'wb'){|f| f.write(Base64.decode64(header_payload))}
     end
-    %x(/opt/cprocsp/bin/amd64/cryptcp -signf -dir "#{path}" -cert -detached -thumbprint "#{ENV['THUMBPRINT']}" -pin "#{ENV['PIN']}" "#{path}/#{filename}")
+    %x(/opt/cprocsp/bin/amd64/cryptcp -signf -dir "#{path}" -cert -detached -thumbprint "#{thumbprint}" -pin "#{ENV['PIN']}" "#{path}/#{filename}")
     sign = File.read("#{path}/#{filename}.sgn").gsub(/\s+/, '')
     send_data({sign: sign}.to_json)
     %x(rm "#{path}/#{filename}")
